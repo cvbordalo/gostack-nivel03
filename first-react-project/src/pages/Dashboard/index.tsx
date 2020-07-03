e6145264097f78725854163293e08f3a84fae78f
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-07-03 00:28:23 -0300

    feat: Dealing with errors

--- first-react-project/src/pages/Dashboard/index.tsx
+++ first-react-project/src/pages/Dashboard/index.tsx
@@ -37,6 +37,7 @@ const Dashboard: React.FC = () => {
 
       setRepositories([...repositories, repository]);
       setNewRepo('');
+      setInputError('');
     } catch (err) {
       setInputError('Cannot find repository');
     }
@@ -47,7 +48,7 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositories on Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
