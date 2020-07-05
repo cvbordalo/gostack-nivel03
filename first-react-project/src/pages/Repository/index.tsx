dbd1bfec144300e51e3339f7fb49fb3fcc3c3f98
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-07-04 22:14:33 -0300

    feat: Add Logo and Back button

--- first-react-project/src/pages/Repository/index.tsx
+++ first-react-project/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/github-logo.svg';
+
+import { Header } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -9,9 +14,14 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-<h1>
-Repository:{params.repository}</h1>
-);
+    <Header>
+      <img src={logoImg} alt="Github Explorer" />
+      <Link to="/dashboard">
+        <FiChevronLeft size={16} />
+        Back
+      </Link>
+    </Header>
+  );
 };
 
 export default Repository;
