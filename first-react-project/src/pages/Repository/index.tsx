a7d5eea1c7086f90be7f4ce81f4803d9353790b1
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-07-04 22:58:17 -0300

    feat: stylize details

--- first-react-project/src/pages/Repository/index.tsx
+++ first-react-project/src/pages/Repository/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/github-logo.svg';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -14,13 +14,53 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <Header>
-      <img src={logoImg} alt="Github Explorer" />
-      <Link to="/dashboard">
-        <FiChevronLeft size={16} />
-        Back
-      </Link>
-    </Header>
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Back
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Opened Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="fsdfkds">
+          <div>
+            <strong>gdfgd</strong>
+            <p>gfdgd</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
